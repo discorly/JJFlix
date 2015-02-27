cb0a2f457f69202ed60feb06b7b1916cbabf08ee
discorly <discorly@gmail.com>
2015-02-27 01:22:49 +0100

    1.04

--- src/jjflix/NewJFrame.java
+++ src/jjflix/NewJFrame.java
@@ -58,10 +58,10 @@ public class NewJFrame extends javax.swing.JFrame {
         Target flixBtn900Fullscreen = new ImageTarget(jflx900full.getAbsoluteFile());
         Target flixBtn1080 = new ImageTarget(jflx1080.getAbsoluteFile());
         Target flixBtn1080Fullscreen = new ImageTarget(jflx1080full.getAbsoluteFile());
-        flixBtn900.setMinScore(0.78);
-        flixBtn900Fullscreen.setMinScore(0.78);
-        flixBtn1080.setMinScore(0.78);
-        flixBtn1080Fullscreen.setMinScore(0.78);
+        flixBtn900.setMinScore(0.9);
+        flixBtn900Fullscreen.setMinScore(0.9);
+        flixBtn1080.setMinScore(0.9);
+        flixBtn1080Fullscreen.setMinScore(0.9);
         targets.add(flixBtn900);
         targets.add(flixBtn900Fullscreen);
         targets.add(flixBtn1080);
